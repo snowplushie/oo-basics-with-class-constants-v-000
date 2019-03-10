686aa0e208e51911524bb810d3442d23a2af7cf1
Paige Wiener <snowplushie@yahoo.com>
2019-03-10 06:06:40 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,12 +2,12 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
   
-  BRAND = []
+  BRANDS = []
 
   def initialize(brand)
     @brand = brand
-    if !BRAND.include?(brand)
-      BRAND << brand
+    if !BRANDS.include?(brand)
+      BRANDS << brand
     end
   end
 
