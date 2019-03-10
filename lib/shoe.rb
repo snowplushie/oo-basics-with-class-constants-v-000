428ef567b5c69726b869f6dcc375491383bf9b80
Paige Wiener <snowplushie@yahoo.com>
2019-03-10 06:01:16 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,12 +1,13 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
+  
+  BRAND = []
 
   def initialize(brand)
     @brand = brand
+    BRAND << brand
   end
-  
-  BRANDS = []
 
   def cobble
     self.condition = "new"
